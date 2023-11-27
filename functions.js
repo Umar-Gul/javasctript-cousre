064020b19dc8d8c7efb571c4f9718f484cdb7ba5
Umar-Gul <gumar4852@gmail.com>
2023-11-27 20:48:28 +0500

    Initial commit

---
+++ functions.js
@@ -0,0 +1,23 @@
+console.log("This is about function in java script");
+
+
+function greet(name,greettext,greet){
+    console.log(name = " is a hardworkin boy")
+    console.log(name + "  " + greettext)
+}
+let name1 = "Umar";
+let name2 = "Ali";
+let name3 = "Usman";
+let name4 = "Abu";
+
+greet (name1,greetText);
+greet (name2,greetText);
+greet (name3,greetText);
+greet (name4,greetText);
+
+
+
+console.log(name1 + "is a good boy")
+console.log(name2 + "  is a good boy")
+console.log(name3 + "is a good boy")
+console.log(name4 + "is a good boy")
